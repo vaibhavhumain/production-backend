349d1ca825613814a8ad8553225071080db9ad6c
Akash Dhiman <akashdhiman96252@gmail.com>
2025-11-27 15:08:47 +0530

    FS

--- server.js
+++ server.js
@@ -8,16 +8,20 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://productiondashboards.netlify.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://productiondashboards.netlify.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   })
 );
 
-// Handle preflight manually (important)
-app.options("*", cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Live");
+});
 
 connectDB();
 
